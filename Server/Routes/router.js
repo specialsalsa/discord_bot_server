a2089169a439a4d514dc8ff78a8610177b206b4d
specialsalsa <48301284+specialsalsa@users.noreply.github.com>
2021-07-04 23:10:30 -0700

    added new routes for server config

--- Server/Routes/router.js
+++ Server/Routes/router.js
@@ -1,29 +1,34 @@
-const express = require('express');
+const express = require("express");
 // creation of the router object
 const router = express.Router();
 
 // import the functions from the Controllers
-const userController = require('../Controllers/userController');
-const ChannelFile = require('../Models/Channel');
-const UserChannelFile = require('../Models/UserChannel');
-const MessageFile = require('../Models/Message');
+const userController = require("../Controllers/userController");
+const ChannelFile = require("../Models/Channel");
+const UserChannelFile = require("../Models/UserChannel");
+const MessageFile = require("../Models/Message");
 
-
-const ServerMovementFile = require('../Models/ServerMovement');
-const ServerMuteFile = require('../Models/ServerMute');
-const ServerShareFile = require('../Models/ServerShare');
-const messageController = require('../Controllers/messageController');
-
+const ServerMovementFile = require("../Models/ServerMovement");
+const ServerMuteFile = require("../Models/ServerMute");
+const ServerShareFile = require("../Models/ServerShare");
+const messageController = require("../Controllers/messageController");
+const configController = require("../Controllers/configController");
 
 // grab the router
 // set a METHOD to it
 // GET
-// POST (add) 
+// POST (add)
 // PUT (update all)
 // PATCH (update 1 or 2 fields only)
 // DELETE (deletes)
 
-router.get('/users', userController.getUsers);
+// get server config
+router.get("/config", configController.getConfig);
+
+// update server config
+router.put("/config/:server_id");
+
+router.get("/users", userController.getUsers);
 
 //...4000/users/4
 router.get("/users/:id", userController.getUserByID);
@@ -51,38 +56,47 @@ router.get("/users-member", userController.getMemberUsers);
 router.get("/users-staff", userController.getStaffUsers);
 
 // get users who have been kicked
-router.get('/users-kicked', userController.getKickedUsers);
+router.get("/users-kicked", userController.getKickedUsers);
 
 // get message count
-router.get('/message-count', messageController.getMessageCount);
+router.get("/message-count", messageController.getMessageCount);
 router.get("/user-messages/:user_id", userController.getMessagesByUserID);
 
-router.post('/user-channel', (req, res) => {
+router.post("/user-channel", (req, res) => {
     userController.addUserToChannel(req, (status, result) => {
         // result = [1, 2]
         if (status == true) {
             res.status(201).send({
                 id_user: result[0],
                 id_channel: result[1]
-            })
+            });
         } else {
-            res.status(400).send('ERROR');
+            res.status(400).send("ERROR");
         }
-    })
+    });
 });
 
-router.get("/messages-by-user", messageController.selectTotalMessagesByUser)
+router.get("/messages-by-user", messageController.selectTotalMessagesByUser);
 
-router.get("/messages-by-user-limit", messageController.selectTotalMessagesByUserWithLimit)
+router.get(
+    "/messages-by-user-limit",
+    messageController.selectTotalMessagesByUserWithLimit
+);
 
-router.get('/messages-by-channel', messageController.selectTotalMessagesByChannel);
+router.get(
+    "/messages-by-channel",
+    messageController.selectTotalMessagesByChannel
+);
 
-router.get('/messages-by/channel/:channel_id', messageController.selectTotalMessagesByChannelID);
+router.get(
+    "/messages-by/channel/:channel_id",
+    messageController.selectTotalMessagesByChannelID
+);
 
-router.get('/messages', messageController.getMessageContent);
+router.get("/messages", messageController.getMessageContent);
 
-router.get('/last-week-messages', messageController.getLastWeekMessages);
+router.get("/last-week-messages", messageController.getLastWeekMessages);
 
-router.post('/message', messageController.newMessage);
+router.post("/message", messageController.newMessage);
 
 module.exports = router;
