270e74e02ca5246c00e625b2c92bc1a9e64c321c
Lyle Wilner <lyleleno@gmail.com>
2022-01-15 20:02:39 -0800

    asdf

--- Server/Database/database.js
+++ Server/Database/database.js
@@ -1,18 +1,20 @@
 // WHERE THE CONNECTION TO THE MYSQL DB IS CREATED
 
-
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: "mysql",
-        logging: false
-    }
-
+const sequelize = new Sequelize(
+  // process.env.DB_NAME
+  'courierbot',
+  // process.env.DB_USER
+  'root',
+  // process.env.DB_PASSWORD,
+  'password',
+  {
+    host: '149.28.70.215',
+    port: 3306,
+    dialect: 'mysql',
+    logging: false
+  }
 );
 
 module.exports = sequelize;
